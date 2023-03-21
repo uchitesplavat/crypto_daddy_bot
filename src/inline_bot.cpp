153c2ee6457d556d447fdc03f672ff3e5afa914a
uchitesplavat <uchitesplavat@mail.ru>
2023-03-22 00:41:05 +0300

    Katharyn_Add_Donate_Button_And_Fix_Top_10_Crypto

--- src/inline_bot.cpp
+++ src/inline_bot.cpp
@@ -7,6 +7,57 @@
 #include <cpr/cpr.h>
 #include <nlohmann/json.hpp>
 #include <iomanip>
+#include <sstream>
+#include "pugixml.hpp"
+
+std::string escapeMarkdownV2(const std::string& input) {
+    std::string result = input;
+    std::string charsToEscape = "_*[]()~`>#+-=|{}.!";
+    for (char c : charsToEscape) {
+        std::string charStr(1, c);
+        size_t pos = result.find(charStr);
+        while (pos != std::string::npos) {
+            result.replace(pos, 1, "\\" + charStr);
+            pos = result.find(charStr, pos + 2);
+        }
+    }
+    return result;
+}
+
+//std::string getNews() {
+//    std::string api_url = "https://www.coindesk.com/feed";
+//    auto response = cpr::Get(cpr::Url{api_url});
+//
+//    if (response.status_code == 200) {
+//        pugi::xml_document doc;
+//        pugi::xml_parse_result result = doc.load_string(response.text.c_str());
+//
+//        if (!result) {
+//            return "Error parsing XML data.";
+//        }
+//
+//        std::string formatted_data;
+//        int news_counter = 1;
+//
+//        for (const auto &item : doc.child("rss").child("channel").children("item")) {
+//            std::string title = item.child_value("title");
+//            std::string url = item.child_value("link");
+//
+//            title = escapeMarkdownV2(title);
+//
+//            formatted_data += std::to_string(news_counter) + ". [" + title + "](" + url + ")\n";
+//            news_counter++;
+//
+//            if (news_counter > 5) {
+//                break;
+//            }
+//        }
+//
+//        return formatted_data;
+//    } else {
+//        return "Error fetching data from CoinDesk.";
+//    }
+//}
 
 std::string getTop10Crypto() {
     std::string api_url = "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=10&page=1&sparkline=false";
@@ -25,7 +76,13 @@ std::string getTop10Crypto() {
             std::stringstream price_stream;
             price_stream << std::fixed << std::setprecision(4) << price;
 
-            formatted_data += "Rank " + market_cap_rank + ": " + name + " (" + symbol + ") - Price: $" + price_stream.str() + "\n";
+            std::string price_string = price_stream.str();
+            // Escape special characters for MarkdownV2
+            price_string = escapeMarkdownV2(price_string);
+
+            // Use MarkdownV2 formatting and escape parentheses, hyphen, and dot in the price
+            // Add double backslashes for newlines
+            formatted_data += "Rank " + market_cap_rank + ": *" + name + "* \\(" + symbol + "\\) \\- Price: $" + price_string + "\n";
         }
 
         return formatted_data;
@@ -34,8 +91,21 @@ std::string getTop10Crypto() {
     }
 }
 
+
+
+bool contains_prohibited_words(const std::string& message, const std::vector<std::string>& prohibited_words) {
+    for (const auto& word : prohibited_words) {
+        if (message.find(word) != std::string::npos) {
+            return true;
+        }
+    }
+    return false;
+}
+
 int main() {
     TgBot::Bot bot("6290187456:AAE5MO_JTBrQHPUfh_hwqW72B1NDC7BIp7g");
+
+    std::vector<std::string> prohibited_words = {"Гей", "гей", "Долбаеб", "долбаеб","scam", "скам", "Скам", "скамье", "Scam", "pidor", "пидор", "пидарас", "Pidor", "Пидор", "Пидарас", "admin", "админ", "Admin", "Админ", "Педик", "Негр", "Нига", "педик", "негр", "нига"};
 
     bot.getEvents().onCommand("help", [&bot](TgBot::Message::Ptr message) {
         TgBot::InlineKeyboardMarkup::Ptr keyboard(new TgBot::InlineKeyboardMarkup);
@@ -52,6 +122,15 @@ int main() {
         button3->text = "TOP 10 CRYPTO";
         button3->callbackData = "top10crypto";
 
+        TgBot::InlineKeyboardButton::Ptr button4(new TgBot::InlineKeyboardButton);
+        button4->text = "DONATE";
+        button4->url = "https://telegra.ph/DONATE-03-21-2";
+
+//        TgBot::InlineKeyboardButton::Ptr button5(new TgBot::InlineKeyboardButton);
+//        button5->text = "NEWS";
+//        button5->callbackData = "news";
+
+
         std::vector<TgBot::InlineKeyboardButton::Ptr> row1;
         row1.push_back(button1);
         row1.push_back(button2);
@@ -59,8 +138,17 @@ int main() {
         std::vector<TgBot::InlineKeyboardButton::Ptr> row2;
         row2.push_back(button3);
 
+        std::vector<TgBot::InlineKeyboardButton::Ptr> row3;
+        row3.push_back(button4);
+
+//        std::vector<TgBot::InlineKeyboardButton::Ptr> row4;
+//        row4.push_back(button5);
+
+
         keyboard->inlineKeyboard.push_back(row1);
         keyboard->inlineKeyboard.push_back(row2);
+        keyboard->inlineKeyboard.push_back(row3);
+//        keyboard->inlineKeyboard.push_back(row4);
 
         bot.getApi().sendMessage(message->chat->id, "В чем бы вам хотелось разобраться?", false, 0, keyboard);
     });
@@ -68,7 +156,20 @@ int main() {
     bot.getEvents().onCallbackQuery([&bot](TgBot::CallbackQuery::Ptr query) {
         if (query->data == "top10crypto") {
             std::string top10CryptoMsg = getTop10Crypto();
-            bot.getApi().sendMessage(query->message->chat->id, top10CryptoMsg);
+            bot.getApi().sendMessage(query->message->chat->id, top10CryptoMsg, false, 0, nullptr, "MarkdownV2");
+        }
+//        if (query->data == "news") {
+//            std::string newsMsg = getNews();
+//            bot.getApi().sendMessage(query->message->chat->id, newsMsg, false, 0, nullptr, "MarkdownV2");
+//        }
+    });
+    bot.getEvents().onAnyMessage([&bot, &prohibited_words](TgBot::Message::Ptr message) {
+        if (contains_prohibited_words(message->text, prohibited_words)) {
+            try {
+                bot.getApi().deleteMessage(message->chat->id, message->messageId);
+            } catch (TgBot::TgException& e) {
+                std::cerr << "Error deleting message: " << e.what() << std::endl;
+            }
         }
     });
 
@@ -86,29 +187,3 @@ int main() {
 
     return 0;
 }
-
-//    bot.getEvents().onCommand("crypto", [&bot](TgBot::Message::Ptr message) {
-//        // Make an HTTP request to the CoinGecko API to retrieve the top 10 cryptocurrency prices
-//        httplib::Client client("api.coingecko.com", 443);
-//        auto res = client.Get("/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=10&page=1&sparkline=false");
-//
-//        // Parse the JSON response
-//        std::vector<std::string> cryptocurrencies;
-//        nlohmann::json_abi_v3_11_2::json response = nlohmann::json_abi_v3_11_2::json::parse(res->body);
-//        for (const auto& item : response) {
-//            std::string name = item["name"];
-//            double price = item["current_price"];
-//            std::string symbol = item["symbol"];
-//            std::stringstream ss;
-//            ss << name << " (" << symbol << "): $" << price;
-//            cryptocurrencies.push_back(ss.str());
-//        }
-//
-//        // Join the cryptocurrency strings together and send them as a message to the user
-//        std::string message_text = "Top 10 cryptocurrencies by market cap:\n\n";
-//        message_text += std::accumulate(cryptocurrencies.begin(), cryptocurrencies.end(), std::string(),
-//                                        [](std::string& a, std::string& b) -> std::string {
-//                                            return a + "\n" + b;
-//                                        });
-//        bot.getApi().sendMessage(message->chat->id, message_text);
-//    });
